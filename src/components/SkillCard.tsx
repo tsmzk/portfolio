0745e5ea2de34e96fa7daff4780f65a7d048aa43
tsmzk <tks.shimazaki.bus@gmail.com>
2024-07-28 16:03:14 +0900

    スキル修正

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,13 +1,30 @@
 import type { ReactNode } from "react";
+import { v4 as uuid } from "uuid";
+
+interface Skill {
+  name: string;
+  experience: string;
+  level: number;
+  optionals?: string[];
+}
 
 interface SkillCardProps {
   title: string;
   description: string;
-  skills: { id: number; name: string; experience: string; level: number }[];
+  skills: Skill[];
   icon: ReactNode;
+  columnName?: string;
 }
 
-export default function SkillCard({ title, description, skills, icon }: SkillCardProps) {
+export default function SkillCard({
+  title,
+  description,
+  skills,
+  icon,
+  columnName,
+}: SkillCardProps) {
+  const hasOptionals = skills.some((skill) => skill.optionals && skill.optionals.length > 0);
+
   return (
     <div className="border p-4 rounded-lg shadow-md">
       <div className="flex items-center mb-4">
@@ -15,20 +32,26 @@ export default function SkillCard({ title, description, skills, icon }: SkillCar
         <h3 className="text-xl font-bold">{title}</h3>
       </div>
       <p className="mb-4">{description}</p>
-      <table className="w-full">
+      <table className="w-full table-fixed">
         <thead>
           <tr>
-            <th className="text-left">技術</th>
-            <th className="text-left">経験年数</th>
-            <th className="text-left">スキル</th>
+            <th className="text-center w-1/4">技術</th>
+            <th className="text-center w-1/4">経験年数</th>
+            {hasOptionals && <th className="text-center w-1/4">{columnName}</th>}
+            <th className="text-center w-1/4">スキル</th>
           </tr>
         </thead>
         <tbody>
           {skills.map((skill) => (
-            <tr key={skill.id}>
-              <td>{skill.name}</td>
-              <td>{skill.experience}</td>
-              <td>
+            <tr key={uuid()} className="align-top border-t border-gray-200">
+              <td className="py-2 text-center">{skill.name}</td>
+              <td className="py-2 text-center">{skill.experience}</td>
+              {hasOptionals && (
+                <td className="py-2 text-center">
+                  {skill.optionals ? skill.optionals.join(", ") : ""}
+                </td>
+              )}
+              <td className="py-2 text-center">
                 <div className="relative w-full h-4 bg-gray-200 rounded-full">
                   <div
                     className="absolute h-4 bg-green-500 rounded-full"
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,42 +1,103 @@
-import { FaDatabase, FaLaptopCode, FaServer } from "react-icons/fa";
+import { FaAws, FaDatabase, FaLaptopCode, FaServer, FaTools } from "react-icons/fa";
+import { v4 as uuid } from "uuid";
 import Section from "./Section";
 import SkillCard from "./SkillCard";
 
 export default function Skills() {
+  const skillCards = [
+    {
+      title: "バックエンド",
+      description:
+        "TypeScriptによるAPI開発がメインです。API技術はRESTとGraphQLの経験があります。Swaggerを用いたAPI設計も経験があります。",
+      skills: [
+        {
+          name: "TypeScript",
+          experience: "4年",
+          level: 3,
+          optionals: ["Express", "Fastify", "NestJS"],
+        },
+        { name: "Go", experience: "1年未満", level: 2, optionals: ["echo"] },
+        { name: "PHP", experience: "1年", level: 1, optionals: ["Laravel"] },
+      ],
+      icon: <FaServer />,
+      columnName: "FW",
+    },
+    {
+      title: "フロントエンド",
+      description:
+        "React/NextによるWebフロントエンド開発がメインです。こちらのサイトも同技術で作成されています。",
+      skills: [
+        {
+          name: "React",
+          experience: "2年",
+          level: 2,
+          optionals: ["Next.js", "styled-components", "Tailwind CSS"],
+        },
+        { name: "HTML/CSS/JS", experience: "1年", level: 2, optionals: ["Scss", "jQuery"] },
+      ],
+      icon: <FaLaptopCode />,
+      columnName: "FW",
+    },
+    {
+      title: "DB",
+      description:
+        "MySQLがメインです。PostgreSQLはコスト軽減のため個人開発でよく利用します。設計はテーブルの既存スキーマの改修から新規作成まで経験があります。",
+      skills: [
+        { name: "MySQL", experience: "4年", level: 3, optionals: ["TypeORM", "Prisma"] },
+        { name: "PostgreSQL", experience: "2年", level: 2, optionals: ["Prisma"] },
+      ],
+      icon: <FaDatabase />,
+      columnName: "ORM",
+    },
+    {
+      title: "インフラ",
+      description:
+        "AWSは実務で各サービスを利用した経験があります。GCPは個人開発で利用しております。",
+      skills: [
+        {
+          name: "AWS",
+          experience: "3年",
+          level: 2,
+          optionals: ["EC2", "Lambda", "API Gateway", "DynamoDB", "OpenSearch"],
+        },
+        {
+          name: "GCP",
+          experience: "1年未満",
+          level: 1,
+          optionals: ["CloudRun", "Cloud Build", "Artifact Registry"],
+        },
+      ],
+      icon: <FaAws />,
+      columnName: "サービス",
+    },
+    {
+      title: "その他ツール等",
+      description: "開発環境やコミュニケーションツールなどの利用経験です",
+      skills: [
+        { name: "Git", experience: "5年", level: 3 },
+        { name: "VScode", experience: "5年", level: 3 },
+        { name: "Slack", experience: "5年", level: 3 },
+        { name: "Microsoft Teams", experience: "1年未満", level: 2 },
+        { name: "Docker", experience: "4年", level: 3 },
+      ],
+      icon: <FaTools />,
+    },
+  ];
+
   return (
     <Section id="skills" title="Skills">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <SkillCard
-          title="バックエンド"
-          description="API開発を開発がメインです"
-          skills={[
-            { id: 1, name: "TypeScript/Node", experience: "4年", level: 3 },
-            { id: 2, name: "Go", experience: "1年", level: 1 },
-            { id: 3, name: "PHP/Laravel", experience: "1年", level: 1 },
-          ]}
-          icon={<FaLaptopCode />}
-        />
-        <SkillCard
-          title="フロントエンド"
-          description="説明"
-          skills={[{ id: 1, name: "React", experience: "2年", level: 1 }]}
-          icon={<FaLaptopCode />}
-        />
-        <SkillCard
-          title="DB"
-          description="説明"
-          skills={[
-            { id: 1, name: "MySQL", experience: "1年", level: 1 },
-            { id: 2, name: "PostgreSQL", experience: "2年", level: 1 },
-          ]}
-          icon={<FaDatabase />}
-        />
-        <SkillCard
-          title="インフラ"
-          description="説明"
-          skills={[{ id: 1, name: "AWS(EC2, RDS等)", experience: "1年", level: 1 }]}
-          icon={<FaServer />}
-        />
+      <div className="flex flex-wrap justify-center gap-4">
+        {skillCards.map((card) => (
+          <div key={uuid()} className="w-full md:w-1/2 lg:w-1/3 flex justify-center">
+            <SkillCard
+              title={card.title}
+              description={card.description}
+              skills={card.skills}
+              icon={card.icon}
+              columnName={card.columnName}
+            />
+          </div>
+        ))}
       </div>
     </Section>
   );
