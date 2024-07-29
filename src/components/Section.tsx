223f8cc355ca245fc787cf8aa364a74ec9e82abc
tsmzk <tks.shimazaki.bus@gmail.com>
2024-07-29 11:41:10 +0900

    見出し調整

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -10,7 +10,7 @@ export default function Section({ id, title, children }: SectionProps) {
   return (
     <section id={id} className="py-8">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-4">{title}</h2>
+        <h2 className="text-7xl font-bold text-center mb-16">{title}</h2>
         {children}
       </div>
     </section>
