75af3becc049518282109be9c3375883628a76da
tsmzk <tks.shimazaki.bus@gmail.com>
2024-07-29 13:57:16 +0900

    fix:lint

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
       className="relative h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/images/top.png')" }}
     >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-50" />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
         <h1 className="text-5xl font-bold mb-4">T.Shimazaki's portfolio site</h1>
         <h2 className="text-2xl">Software Engineer</h2>
