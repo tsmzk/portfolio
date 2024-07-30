ebbaea8932682a20ed28fd8182fb40cb04d41740
tsmzk <tks.shimazaki.bus@gmail.com>
2024-07-30 16:07:34 +0900

    fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,23 +1,31 @@
-import { AiOutlineX } from 'react-icons/ai';
-import { BiLinkExternal } from 'react-icons/bi';
-import Section from './Section';
+import { AiOutlineX } from "react-icons/ai";
+import { BiLinkExternal } from "react-icons/bi";
+import Section from "./Section";
 
 export default function Contact() {
   return (
     <Section id="contact" title="Contact">
       <div className="container mx-auto text-center">
-        <p className="mb-8">
-          お問い合わせやお仕事依頼は以下からDM等にてお願いします！
-        </p>
+        <p className="mb-8">お問い合わせやお仕事依頼は以下からDM等にてお願いします！</p>
         <div className="flex justify-center space-x-8">
           <div className="flex flex-col items-center">
-            <a href="https://x.com/zakiShimyan" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+            <a
+              href="https://x.com/zakiShimyan"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+            >
               <AiOutlineX size="3em" className="text-blue-500 hover:text-blue-700" />
             </a>
             <p className="mt-2">X</p>
           </div>
           <div className="flex flex-col items-center">
-            <a href="https://www.wantedly.com/id/zakiShima" target="_blank" rel="noopener noreferrer" aria-label="Wantedly">
+            <a
+              href="https://www.wantedly.com/id/zakiShima"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Wantedly"
+            >
               <BiLinkExternal size="3em" className="text-blue-500 hover:text-blue-700" />
             </a>
             <p className="mt-2">Wantedly</p>
