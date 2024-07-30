1d6ae3ff398ec9f1c83bb866972b42f6f516454c
tsmzk <tks.shimazaki.bus@gmail.com>
2024-07-30 16:10:00 +0900

    react-scroll導入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,24 @@
+"use client";
+
+import { Link as ScrollLink } from 'react-scroll';
+
 export default function Header() {
   return (
     <header className="bg-gray-900 text-white fixed top-0 w-full z-20">
       <div className="container mx-auto px-4 py-2 flex justify-center">
         <nav className="space-x-8 text-lg flex">
-          <a href="#about" className="hover:text-gray-300">
+          <ScrollLink to="about" smooth={true} duration={500} className="hover:text-gray-300 cursor-pointer">
             About me
-          </a>
-          <a href="#skills" className="hover:text-gray-300">
+          </ScrollLink>
+          <ScrollLink to="skills" smooth={true} duration={500} className="hover:text-gray-300 cursor-pointer">
             Skills
-          </a>
-          <a href="#works" className="hover:text-gray-300">
+          </ScrollLink>
+          <ScrollLink to="works" smooth={true} duration={500} className="hover:text-gray-300 cursor-pointer">
             Works
-          </a>
-          <a href="#contact" className="hover:text-gray-300">
+          </ScrollLink>
+          <ScrollLink to="contact" smooth={true} duration={500} className="hover:text-gray-300 cursor-pointer">
             Contact
-          </a>
+          </ScrollLink>
         </nav>
       </div>
     </header>
