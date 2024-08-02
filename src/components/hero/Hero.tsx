1309bb37d3ff7fb55b315f7786ed739fa9c4e378
tsmzk <tks.shimazaki.bus@gmail.com>
2024-08-02 17:11:11 +0900

    fix

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
     >
       <div className="absolute inset-0 bg-black opacity-50" />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
-        <h1 className="text-5xl font-bold mb-4">T.Shimazaki's portfolio site</h1>
+        <h1 className="text-5xl font-bold mb-4">T.Shimazaki's portfolio</h1>
         <h2 className="text-2xl">Software Engineer</h2>
       </div>
     </div>
